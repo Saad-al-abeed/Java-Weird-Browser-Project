1ac33175e5ccc05b7a1dbc991a890d6098ce62a4
Saad-al-abeed <saadalabeed1737@gmail.com>
2026-04-23 16:53:56 +0600

    removed unused imports from DomParser

--- src/main/java/org/custombrowser/domparser/DomParser.java
+++ src/main/java/org/custombrowser/domparser/DomParser.java
@@ -2,8 +2,6 @@ package org.custombrowser.domparser;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 public class DomParser {
 
